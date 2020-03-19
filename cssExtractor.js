ec03ce044e9244fd9eb873d86c8b8e8f5730e42b
Emmanuel <meric.emmanuel@gmail.com>
2020-03-19 17:29:17 +0100

    fixed bug

--- cssExtractor.js
+++ cssExtractor.js
@@ -2,7 +2,17 @@ function extractClassnames(cssAST) {
   const rules = cssAST.stylesheet.rules;
   let selectors = [];
   for (const rule of rules) {
-    selectors = [...selectors, ...rule.selectors];
+    switch (rule.type) {
+      case 'media':
+        for (const subRule of rule.rules) {
+          selectors = [...selectors, ...subRule.selectors]
+        }
+        break;
+      case 'keyframes':
+        break;
+      default:
+        selectors = [...selectors, ...rule.selectors];
+    }
   }
 
   const classnameRegex = /\.[a-zA-Z0-9\-_]+/g;
